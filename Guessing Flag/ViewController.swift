fdef4536918d5f1643f6f3bb9d905aeb7b9c814c
Justine Kenji Dela Cruz <kenjidckenji@gmail.com>
2022-11-09 18:29:45 -0500

    added challenges

--- Guessing Flag/ViewController.swift
+++ Guessing Flag/ViewController.swift
@@ -8,12 +8,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
+    var attempts = 0
     override func viewDidLoad() {
         super.viewDidLoad()
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco","nigeria", "poland", "russia", "spain", "uk", "us"]
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        
+        askQuestion()
         // Do any additional setup after loading the view.
     }
-
-
+    func askQuestion(action: UIAlertAction! = nil){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased() + " Score: \(score)"
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        var message: String
+        
+        if sender.tag == correctAnswer{
+            title = "Correct"
+            score += 1
+            message = "Your score is \(score)"
+        }else{
+            title = "Wrong"
+            score -= 1
+            message = "Your score is \(score), correct answer is \(countries[correctAnswer])"
+        }
+        attempts += 1
+        if attempts == 9{
+            title = "Finished!"
+            message = "Your final score is \(score)"
+            score = 0
+            attempts = 0
+        }
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
+    }
+    
 }
 
---
+++ Terminal.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#  Terminal.sh
+#  Guessing Flag
+#
+#  Created by Justine kenji Dela Cruz on 09/11/2022.
+#
+open -a Terminal .
